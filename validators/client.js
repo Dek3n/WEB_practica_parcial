d31345462e5dfb67275b31b6d63143bcf62efa2c
Dek3n <148790347+Dek3n@users.noreply.github.com>
2025-04-23 16:03:58 +0200

    Validaciones de Cliente
    ¿Qué vamos a validar?
    
    name	Obligatorio
    nif	Obligatorio, mínimo 8 caracteres
    email	Si existe, que sea un email válido
    phone	(opcional) mínimo 9 caracteres

---
+++ validators/client.js
@@ -0,0 +1,23 @@
+import { check } from "express-validator";
+import validateResults from "../utils/handleValidator.js";
+
+const validatorCreateClient = [
+  check("name").exists().notEmpty().withMessage("El nombre es obligatorio"),
+  check("nif").exists().notEmpty().isLength({ min: 8 }).withMessage("El NIF debe tener al menos 8 caracteres"),
+  check("email").optional().isEmail().withMessage("El correo debe ser válido"),
+  check("phone").optional().isLength({ min: 9 }).withMessage("El teléfono debe tener al menos 9 caracteres"),
+  (req, res, next) => validateResults(req, res, next)
+];
+
+const validatorUpdateClient = [
+  check("name").optional().notEmpty(),
+  check("nif").optional().isLength({ min: 8 }),
+  check("email").optional().isEmail(),
+  check("phone").optional().isLength({ min: 9 }),
+  (req, res, next) => validateResults(req, res, next)
+];
+
+export {
+  validatorCreateClient,
+  validatorUpdateClient
+};
